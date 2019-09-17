76cb55ce92177ed33029b5f4b2b4c1d36588d648
shilangyu <xmarcinmarcin@gmail.com>
2019-09-17 22:45:12 +0200

    added godoc

---
+++ doc.go
@@ -0,0 +1,23 @@
+/*
+Package main is a library for creating blockchains.
+	package main
+
+	import (
+		"fmt"
+
+		"github.com/shilangyu/go-chain"
+	)
+
+	func main() {
+		bc := chain.NewBlockChain()
+		bc.AddBlock([]byte("Second block"))
+		bc.AddBlock([]byte("Third block"))
+
+		fmt.Printf("The blockchain is correct: %t", bc.Validate())
+
+		bc[0].data = []byte("altered")
+
+		fmt.Printf("The blockchain is correct: %t", bc.Validate())
+	}
+*/
+package main
