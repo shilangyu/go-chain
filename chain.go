fec060be807d461664d675be4fccb0611e9e4fe0
shilangyu <xmarcinmarcin@gmail.com>
2019-09-17 16:09:43 +0200

    created chain

---
+++ chain.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"bytes"
+	"crypto/sha256"
+)
+
+// BlockChain is a slice of Blocks
+// it is recommended to initialize it from NewBlockChain, not manually
+type BlockChain []Block
+
+// NewBlockChain initializes a BlockChain with a genesis block
+func NewBlockChain() []Block {
+	block := Block{
+		prevHash: []byte{},
+		data:     []byte("GENESIS"),
+	}
+	hash := sha256.Sum256(bytes.Join([][]byte{block.data, block.prevHash}, []byte{}))
+	block.hash = hash[:]
+
+	return append(BlockChain{}, block)
+}
