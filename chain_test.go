8efb5b5f9c522158f7360cf0323483f86f38d527
shilangyu <xmarcinmarcin@gmail.com>
2019-09-17 21:59:48 +0200

    testing stringer

--- chain_test.go
+++ chain_test.go
@@ -68,3 +68,32 @@ func TestBlockChain_Validate(t *testing.T) {
 		})
 	}
 }
+
+func TestBlockChain_String(t *testing.T) {
+	bc := NewBlockChain()
+	bc.AddBlock([]byte("block 2"))
+
+	tests := []struct {
+		name string
+		bc   BlockChain
+		want string
+	}{
+		{
+			"Bare",
+			NewBlockChain(),
+			"[GENESIS]",
+		},
+		{
+			"2 blocks",
+			bc,
+			"[GENESIS -> block 2]",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := tt.bc.String(); got != tt.want {
+				t.Errorf("BlockChain.String() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
