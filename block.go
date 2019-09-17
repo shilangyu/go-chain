91dc40dd7a617b13901d07dea15f93c830ce6e1c
shilangyu <xmarcinmarcin@gmail.com>
2019-09-17 16:03:23 +0200

    create block

---
+++ block.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"bytes"
+	"crypto/sha256"
+)
+
+// Block is an element in a blockchain
+// it is recommended to initialize it from NewBlock, not manually
+type Block struct {
+	prevHash []byte
+	data     []byte
+	hash     []byte
+}
+
+// NewBlock initializes a block derived from
+// the previous block and given data
+func NewBlock(prevBlock Block, data []byte) *Block {
+	hash := sha256.Sum256(bytes.Join([][]byte{prevBlock.prevHash, data}, []byte{}))
+
+	return &Block{
+		prevBlock.hash,
+		data,
+		hash[:],
+	}
+}
